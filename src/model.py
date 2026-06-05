de44bbd027b65999aadf6c9059241aa36b223daf
Kacper Drabicki <kdrabicki23@gmail.com>
2026-06-05 20:06:05 +0200

    Added main.py and src/model.py. Defined the model architecture in src/model.py.

---
+++ src/model.py
@@ -0,0 +1,20 @@
+import torch.nn as nn
+
+class Bouncer(nn.Module):
+
+    def __init__(
+            self,
+            input_dim: int = 1,
+            hidden_dim: int = 10,
+            output_dim: int = 1,
+            activation: nn.Module = nn.Tanh()
+    ):
+        super().__init__()
+        self.network = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            activation,
+            nn.Linear(hidden_dim, output_dim)
+            )
+        
+    def forward(self, x):
+        return self.network(x)
